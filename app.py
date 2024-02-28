210b4faddcdfb5229726a7a8396bb0e8e3148b5f
Aktonen <aku.tolonen1999@hotmail.fi>
2024-02-28 13:45:36 +0200

    Removed the GET. We can do this in react.

--- app.py
+++ app.py
@@ -10,21 +10,6 @@ db = firestore.client()
 
 app = Flask(__name__)
 
-@app.route("/get_data", methods=["GET"])
-def get_data():
-    try:
-        # Get all documents from the "news" collection
-        docs = db.collection("news").get()
-
-        # Convert documents to a list of dictionaries
-        data = [doc.to_dict() for doc in docs]
-
-        return f"Retrieved data: {data}", 200
-    except Exception as e:
-        # Log the error for better debugging
-        print(f"Error retrieving data: {str(e)}")
-        return "Error retrieving data", 500
-
 @app.route("/update_data", methods=["POST"])
 def update_data():
     # Get data from request
