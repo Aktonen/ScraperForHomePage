f61b9e94145f736626e72bdc73c58d8122fe54dd
Aktonen <aku.tolonen1999@hotmail.fi>
2024-03-03 09:12:36 +0200

    Added API_KEY

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 import firebase_admin
 from firebase_admin import firestore
 from scraper import get_scraped_data
+import os
 
 # Initialize Firebase app
 cred = firebase_admin.credentials.Certificate("db.json")
@@ -10,26 +11,34 @@ db = firestore.client()
 
 app = Flask(__name__)
 
+API_KEY = os.environ.get('API_KEY')
+
 @app.route("/update_data", methods=["POST"])
 def update_data():
-    # Get data from request
-    scraped_data = get_scraped_data()
 
-    query = db.collection("news")
+    if request.headers.get('Authorization') != API_KEY:
 
-    try:
-        for item in scraped_data:
-            # Check if the item's "link" already exists in the collection
-            existing_item = query.where("link", "==", item["link"]).get()
+        return jsonify({'error': 'Unauthorized'}), 401
+    
+    elif request.headers.get('Authorization') == API_KEY:
+        # Get data from request
+        scraped_data = get_scraped_data()
 
-            if not existing_item:  # If no existing item found, add the new one
-                db.collection("news").add(item)
+        query = db.collection("news")
 
-        return "Data saved successfully", 200
-    except Exception as e:
-        # Log the error for better debugging
-        print(f"Error saving data: {str(e)}")
-        return "Error saving data", 500
+        try:
+            for item in scraped_data:
+                # Check if the item's "link" already exists in the collection
+                existing_item = query.where("link", "==", item["link"]).get()
+
+                if not existing_item:  # If no existing item found, add the new one
+                    db.collection("news").add(item)
+
+            return "Data saved successfully", 200
+        except Exception as e:
+            # Log the error for better debugging
+            print(f"Error saving data: {str(e)}")
+            return "Error saving data", 500
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
